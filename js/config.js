c2cc5317fc867f75700f25dabaf949b7771ed031
Donald Kelsey <dmkelseyII@gmail.com>
2019-07-08 10:01:56 -0500

    Fix credentials

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_2u4BsAuB7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'athimn1frqk5qplmhgrb83mq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'iathimn1frqk5qplmhgrb83mq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
